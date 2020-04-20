add46b76ecf28c2b4300f6aa989895c25fc099c9
Ioan <john.tirsu@yahoo.com>
2020-04-20 20:17:17 +0300

    JS Data Type

---
+++ script.js
@@ -0,0 +1,20 @@
+let userName = 'Ioan';
+
+let x = 100;
+let y = 200;
+let z;
+console.log(typeof z)
+z = x + y;
+
+let YES = true;
+
+let userDoB = null;
+
+let numericArray = [1, 2, 3, 4];
+
+alert(userName);
+console.log(typeof userName)
+console.log(typeof z)
+console.log(typeof YES)
+console.log(typeof userDoB)
+console.log(typeof numericArray)
